71cfc83947e78ea197484673a4d3146f29235973
Ankit Sharma <145925577+ankitSharma645@users.noreply.github.com>
2025-09-30 14:11:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Hardcoded for testing
+  origin: 'https://work-flow-pro-assignment.netlify.app', // Hardcoded for testing
   credentials: true
 }));
 app.use(express.json());
